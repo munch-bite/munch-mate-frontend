10bfe74e04c627272c4bad32caad1c6725345e94
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-08-01 17:00:59 +0000

    started homepage

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,8 @@
  import Favicon from "../images/favicon.png"
  import Logo from "../images/munchlogo.png"
+ import HomeImg from '../images/homeImg.png'
 
 
  export const images ={
-    Favicon, Logo
+    Favicon, Logo, HomeImg
  }
--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import NavBar from '../../components/navBar'
+import { images } from '../../assets/images'
 
 const Landing = () => {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+
+      <div className='h-[34vw] mx-auto my-[30px] header relative flex'>
+
+        <div className='header-contents absoute flex fex-col items-start gap-[1.5vw] max-w-[50%] b-[10%] left-[6vw]'>
+          <h2>Order your favourite food here</h2>
+          <p>Choose from a diverse menu featuring a delectable many of dishes crafted with </p>
+          <button>View Menu</button>
+
+        </div>
+
+        <img src={images.HomeImg} alt="eader image" />
       </div>
+    </div>
   )
 }
 

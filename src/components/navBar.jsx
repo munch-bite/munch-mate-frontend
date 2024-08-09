ce89e6b5523e743cd08539a640372e322224ff18
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-08-09 13:16:21 +0000

    cart

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -8,9 +8,9 @@ const NavBar = ({ setShowLogin }) => {
 
   return (
     <div className='padding flex justify-between items-center w-[80%] m-auto'>
-      <Link to='/'>
+      <a href='/'>
         <img src={images.Logo} alt="Logo" className='logo h-8 w-[150px]' />
-      </Link>
+      </a>
       <ul className='padding-menu flex list-none gap-[20px] text-green-950 text-[18px] cursor-pointer'>
         <li onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</li>
         <li onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</li>
@@ -19,9 +19,9 @@ const NavBar = ({ setShowLogin }) => {
       <div className='padding-right flex items-center gap-[40px]'>
         <Search className='img' />
         <div className='padding-search relative'>
-          <Link to='/cart'>
+          <a href='/cart'>
             <ShoppingBasket className='img' />
-          </Link>
+          </a>
           <div className='dot absolute min-w-[10px] min-h-[10px] bg-[#EA7519] rounded-full top-[-8px] right-[-8px]'></div>
         </div>
         <button onClick={() => setShowLogin(true)} className='bg-transparent text-[16px] text-green-950 border-[1px] border-solid p-2 border-r[50px] cursor-pointer rounded-full hover:bg-[#EA7519] transition-[0.5s]'>sign in</button>
